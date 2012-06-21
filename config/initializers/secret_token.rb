fded91b9a1d5b34e0f3a3a2b143f20f56a43138e
Jonathan <Jonathan@nullbyte.net>
2012-06-21 14:46:41 +0200

    lt är för seg så jag börjar på en rails version av sidan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dreamcatchr::Application.config.secret_token = '65e130c7410d23fbecfc400eaa036ba862033fc2ce3ffda20d185a149a7664f2a3fc61642c2a6c767ebdd5caa7fd48fbfc35174539b3ea05948525d189fbedff'
